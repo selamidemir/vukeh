636cf13b247cce17d4296ce8d0613e64e4efd3da
Selami Demir <selamidemir@gmail.com>
2022-09-06 18:18:17 +0300

    First commit, uygulama çatısı oluşturuldu

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+        <h2>Vücut Kitle Endeksi Nedir?</h2>
+        <div></div>
+    </div>
+  );
+}
+
+export default About;
